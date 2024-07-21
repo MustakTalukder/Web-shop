43d8678320cddae8b00233189e8747d417079418
ShimulAUST <shimulpaul59@gmail.com>
2024-07-21 17:07:22 +0200

    update in product image upload working-1

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -147,6 +147,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
