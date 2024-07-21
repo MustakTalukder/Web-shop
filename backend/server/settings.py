c9bb5ef1007b8ceaed768d4b79d30baf11f348ef
ShimulAUST <shimulpaul59@gmail.com>
2024-07-21 16:48:36 +0200

    update in admin panel - js issue solved- v2

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -144,7 +144,9 @@ USE_TZ = True
 
 # STATIC_URL = "/staticfiles/"
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
