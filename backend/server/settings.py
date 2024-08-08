688e6b5518e717975c1112118d57cb3c50824215
mustak <mustakhridoy@gmail.com>
2024-08-09 01:51:28 +0200

    cors allowed origin added

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -36,6 +36,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://0.0.0.0',
     "http://127.0.0.1:3000",
     "http://localhost:3000",
+    "http://20.86.25.223:3000",
+    "http://20.86.25.223"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
