703d6fe5fc5cdfe4305027cbbc3ca46f10acba95
Sayeed843 <sayeed.swe@gmail.com>
2024-08-08 14:16:59 +0200

    New Data

--- backend/core/api/views.py
+++ backend/core/api/views.py
@@ -140,3 +140,6 @@ def create_order(request):
     except Exception as e:
         print("Exception:", str(e))
         return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+    # Comment Code
